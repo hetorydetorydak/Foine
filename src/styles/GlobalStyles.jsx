6a9f8dcfcd520aadbb0330d431787ee6cdcdbff9
hetorydetorydak <johnhextor312@gmail.com>
2025-11-19 06:58:57 +0800

    refactor(styles): removed import fonts from globalstyles.jsx and moved to index.css

--- src/styles/GlobalStyles.jsx
+++ src/styles/GlobalStyles.jsx
@@ -2,7 +2,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Brawler:wght@400;500;600;700&display=swap');
   
   * {
     margin: 0;
