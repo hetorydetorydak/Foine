0796ab09e9167e0fb5e6fa0539950ef67e075c40
Adam Cloyd H. Esparcia <adamcloydesparcia@gmail.com>
2025-11-21 08:43:28 +0800

    Revert "refactor(foine): delete unnecessary pages, Login, Registration, and Home"
    This reverts commit 9cd182598bd85912286048a88ec0109687dbd86c.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './styles/GlobalStyles';
 import HomePage from './pages/HomePage';
 import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Registration from './pages/Registration';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/landing" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
         </Routes>
       </Router>
     </>
